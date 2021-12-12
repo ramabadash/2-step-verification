76ef5714b57f731098c20788afe4f1fd7905410d
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-12-12 21:21:26 +0200

    # Change the navigation from login to the page from the response address

--- 2-step-varification/src/components/login/Login.jsx
+++ 2-step-varification/src/components/login/Login.jsx
@@ -42,7 +42,7 @@ export default function Login() {
             const userName = userNameLoginInput.current.value;
             const password = passwordLoginInput.current.value;
             const answer = await login(userName, password);
-            if (answer) navigate('/home');
+            if (answer) navigate(`/${answer}`);
           } catch (error) {
             notyf.error(`Sorry, someThing went wrong. please try again!`); //error message
           }
