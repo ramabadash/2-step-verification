96a7ad90c40bb17d8a5377b950bb73e4064a8bc7
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-12-12 18:17:24 +0200

    # Create user context with login function and states for userName and loggedIn

---
+++ 2-step-varification/src/components/UserContext.jsx
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+import { BASEURL } from './App.jsx';
+import { Notyf } from 'notyf';
+import 'notyf/notyf.min.css';
+const notyf = new Notyf();
+
+export const UserContext = React.createContext({});
+
+export const UserProvider = ({ children }) => {
+  const [userName, setUserName] = useState(null);
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  // Send login form
+  const login = async (userName, password) => {
+    try {
+      if (!userName || !password) {
+        return notyf.error(`Sorry, Missing details. please try again!`); //error message
+      }
+      const response = await axios.post(`${BASEURL}/users/login`, {
+        userName,
+        password: String(password),
+      });
+      setUserName(userName);
+      setLoggedIn(true);
+      notyf.success('Welcome!'); //success message
+      return response.data;
+    } catch (error) {
+      notyf.error(`Sorry, ${error.response.data}. please try again!`); //error message
+    }
+  };
+
+  return <UserContext.Provider value={{ loggedIn, userName, login }}>{children}</UserContext.Provider>;
+};
