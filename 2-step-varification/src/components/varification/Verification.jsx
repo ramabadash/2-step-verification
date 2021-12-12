e17e2025386589413ec0203bf0fd6559a95c9489
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-12-12 22:37:05 +0200

    # Add token code validation by API req from the "Verification" component

--- 2-step-varification/src/components/varification/Verification.jsx
+++ 2-step-varification/src/components/varification/Verification.jsx
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { useContext, useRef } from 'react';
+import axios from 'axios';
+import { UserContext } from '../UserContext.jsx';
+import { BASEURL } from '../App.jsx';
+import { useNavigate } from 'react-router';
 import './Verification.css';
+import { Notyf } from 'notyf';
+import 'notyf/notyf.min.css'; // for React, Vue and Svelte
+const notyf = new Notyf();
 
 export default function Verification() {
+  /***** STATES ******/
+  const { userName } = useContext(UserContext);
+
+  /***** REFS ******/
+  const codeInput = useRef(null);
+
+  /***** FUNCTIONS ******/
+  // Navigation function
+  const navigate = useNavigate();
+  // Validate from app code
+  const validateCode = async () => {
+    try {
+      const code = codeInput.current.value;
+      const response = await axios.post(`${BASEURL}/users/verification-login`, { userName, code });
+      if (response.data) {
+        navigate(`/${response.data}`);
+      }
+    } catch (error) {
+      notyf.error(`Sorry, ${error}. please try again!`); //error message
+    }
+  };
+
   return (
     <div id='verification-div'>
       <h1>Verification page :</h1>
-      <input type='text' placeholder='Enter the code from your app here...' />
-      {/* TODO - ADD CODE VALIDATION API FUNCTION */}
-      <button className='login-btn'>LOGIN</button>
+      <input ref={codeInput} type='text' placeholder='Enter the code from your app here...' />
+      <button className='login-btn' onClick={validateCode}>
+        LOGIN
+      </button>
     </div>
   );
 }
